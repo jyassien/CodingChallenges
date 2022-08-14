ac89e27a8902a37b6cd5c5a31348f26415d79f5d
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-08-13 22:11:48 -0600

    Optimized sorting algorithm added.

--- HackerRank/sortAndMiddle/sortAndMiddle.js
+++ HackerRank/sortAndMiddle/sortAndMiddle.js
@@ -2,6 +2,7 @@ function sortAndMiddle(arr) {
   let count = 0;
   for (let j = 0; j < arr.length; j++) {
     for (let i = 0; i < arr.length + 1; i++) {
+      count++;
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i + 1];
         arr[i + 1] = arr[i];
@@ -11,12 +12,48 @@ function sortAndMiddle(arr) {
   }
   let midIndex = Math.trunc(arr.length / 2);
   midIndex = midIndex < arr.length / 2 ? midIndex + 1 : midIndex;
-  medianValue = arr.length % 2 == 1? arr[midIndex]: (arr[midIndex]+arr[midIndex + 1])/2;  
+  let medianValue =
+    arr.length % 2 == 1
+      ? arr[midIndex - 1]
+      : (arr[midIndex] + arr[midIndex + 1]) / 2;
+  console.log(" Arr Length: ", arr.length);
   console.log(" Mid Index: ", midIndex);
   console.log(" Sorted Arr: ", arr);
   console.log(" Mid Element: ", arr[midIndex - 1]);
   console.log(" Median Value: ", medianValue);
+  console.log(" Number of iteration: ", count);
 }
 
-let arr = [5, 23, 2, 4, 6, 1, 67, 3, 3, 9, 0,-55,8];
+let arr = [5, 23, 2, 4, 6, 1, 67, 3, 3, 9, 0, -55, 8, 8];
 sortAndMiddle(arr);
+//////////// Output //////
+// Arr Length:  14
+//  Mid Index:  7
+//  Sorted Arr:  [-55,  0, 1, 2, 3, 3,4,  5, 6, 8, 8, 9, 23, 67]
+//  Mid Element:  4
+//  Median Value:  5.5
+//  Number of iteration:  210
+
+function optimizedSort(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      count++;
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  console.log("Sorted Array: ", arr);
+  console.log("Number of Iteration: ", count);
+}
+
+optimizedSort(arr);
+
+//////////// Output //////
+// Sorted Array:  [ -55,  0, 1, 2, 3, 3, 4,  5, 6, 8, 8, 9,23, 67 ]
+//   Number of Iteration:  91
