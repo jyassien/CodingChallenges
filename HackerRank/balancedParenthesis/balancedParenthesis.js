053df86286aa0c6137d7f4b89535661da0a1f7f0
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-10-18 16:15:05 -0600

    balancedParenthesis

---
+++ HackerRank/balancedParenthesis/balancedParenthesis.js
@@ -0,0 +1,23 @@
+function balancedParenthesis(str) {
+    let openPar = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i]=="(" ) openPar++;
+        if(str[i] == ")" ) {
+           if(openPar > 0) {
+               openPar--;
+           }else{
+               return false;
+           }
+        }     
+    }
+    
+    if (openPar == 0) return true;
+    
+  return false;  
+}
+
+console.log(balancedParenthesis('')); // true
+console.log(balancedParenthesis('(ad((h)ljkle))')); // true
+console.log(balancedParenthesis('))((')); // false
+console.log(balancedParenthesis('(())')); // true
+console.log(balancedParenthesis('((hrdh)')); // false
