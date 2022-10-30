f2a088c35f7893d29255947d03b7066a0861748d
Jihadu Yassien <65809610+jyassien@users.noreply.github.com>
2022-10-29 21:18:45 -0600

    balancedContainers updated

--- HackerRank/balancedParenthesis/balancedContainers.js
+++ HackerRank/balancedParenthesis/balancedContainers.js
@@ -1,32 +1,51 @@
 function balancedContainers(str) {
-    let openPar = [];
-    for(let i = 0; i < str.length; i++){
-        let current = str[i]
-        if(current == "(" || current == "{" || current == "[") openPar.push(current);
-       else if(current == ")" ) {
-           if(openPar.includes('(')) {
-               // remove '(' from array openPar
-               // 
-           }
-           else return false
-       }  
-       else if(current == "}" ) {
-           if(openPar.includes('{')) {
-               // remove '(' from array openPar
-               //
-           }
-           else return false
-       }  
-       else if(current == "]" ) {
-           if(openPar.includes('[')) {
-               // remove '(' from array openPar
-               //
-           }
-           else return false
-       }  
-      
-          
+  let openCont = [];
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    if (current == "(" || current == "{" || current == "[")
+      openCont.push(current);
+
+    else if (current == ")") {
+      if (openCont.includes("(")) {
+        // remove '(' from array openCont
+        let parIndex = openCont.indexOf("("); //get  "(" index
+        //remove '(' from the openCont array
+        openCont.splice(parIndex, 1);  
+
+      } else return false;
+    } else if (current == "}") {
+      if (openCont.includes("{")) {
+        // remove '{' from array openCont
+        let curIndex = openCont.indexOf("{"); //get  "{" index
+        //remove '{' from the openCont array
+        openCont.splice(curIndex, 1); 
+
+      } else return false;
+    } else if (current == "]") {
+      if (openCont.includes("[")) {
+        // remove '[' from array openCont
+        let braIndex = openCont.indexOf("["); //get  "[" index
+        //remove '[' from the openCont array
+        openCont.splice(braIndex, 1); 
+        
+      } else return false;
     }
-    if (openPar.length == 0) return true;
+  }
+  if (openCont.length == 0) return true;
   return false;
 }
+
+
+console.log(balancedContainers('(())'));  // true
+console.log(balancedContainers('{{}}'));  // true
+console.log(balancedContainers('}}{{'));  // false
+console.log(balancedContainers('[[]]'));  // true
+
+console.log(balancedContainers('[()]'));  // true
+console.log(balancedContainers('[([{}])]'));  // true
+
+console.log(balancedContainers('[[{}])]'));  // false
+console.log(balancedContainers('[([{])]'));  // false
+
+console.log(balancedContainers('[([{])}]'));  // true ,but it hould be false
+console.log(balancedContainers('[(}[{]))]'));  // false
